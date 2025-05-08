df1b358f58ce0028fb253e5228cc558b51a94e9c
daniyelford <29danialfrd69@gmail.com>
2025-05-07 19:34:05 -0700

    remove manager

--- page editor/client/application/config/constants.php
+++ page editor/client/application/config/constants.php
@@ -84,4 +84,7 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 const DS = DIRECTORY_SEPARATOR;
-const SITE_SERVER="daniyalfrd.ir";
+const DBNAME = 'landing';
+const USERNAME = 'root';
+const PASSWORD = '';
+const SITE_SERVER="manager";
