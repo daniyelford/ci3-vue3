a5b9ef0ffea7783d6ea0df0236018b15daa05310
daniyelford <29danialfrd69@gmail.com>
2025-05-07 19:47:44 -0700

    h

--- page editor/client/application/models/Api_model.php
+++ page editor/client/application/models/Api_model.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exut('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 class Api_model extends CI_Model
 {
     public $table='tbl_sample';
