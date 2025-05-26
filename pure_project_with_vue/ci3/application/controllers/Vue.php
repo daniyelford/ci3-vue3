4c09a58f1b758665c1978daee8f3e52d6f1a4d76
daniyelford <29danialfrd69@gmail.com>
2025-05-26 05:43:05 +0330

    last test

--- pure_project_with_vue/ci3/application/controllers/Vue.php
+++ pure_project_with_vue/ci3/application/controllers/Vue.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Vue extends CI_Controller {
 	public function index(){
-        redirect('assets/');
-		// $this->load->view('vue');
+        // redirect('assets/');
+		$this->load->view('dist/vue');
 	}
 }
--- pure_project_with_vue/vue/vue.config.js
+++ pure_project_with_vue/vue/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: '/codeigniter/pure_project_with_vue/assets/',
+  publicPath: '/codeigniter/pure_project_with_vue/',
   transpileDependencies: true
 })
